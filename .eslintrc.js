6cf250c921c1ee01e03cfc6e8727498d6158e457
swalha1999 <mohamd514.m@gmail.com>
2022-10-19 01:59:57 +0300

    turn off eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   plugins: ['prettier'],
   extends: ['next/core-web-vitals'],
   rules: {
-    'no-console': 'error',
-    'prettier/prettier': 'warn',
+    'no-console': 'off',
+    'prettier/prettier': 'off',
     'react-hooks/exhaustive-deps': 'off',
     'react/display-name': 'off',
     'import/no-anonymous-default-export': 'off',
