04168482ad678314fbbf0ff6276609dae778538b
swalha1999 <mohamd514.m@gmail.com>
2022-10-19 14:57:56 +0300

    added command allc to list all availbe commands

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -31,6 +31,12 @@ export const help = async (args: string[]): Promise<string> => {
   `;
 };
 
+export const allc = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
+};
+
+
 export const whoami = async (args: string[]): Promise<string> => {
   return 'Falcon';
 };
