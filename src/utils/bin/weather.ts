4ab3b2bff431571f374a0de831c3c8f2008f0b7f
swalha1999 <mohamd514.m@gmail.com>
2022-10-19 15:06:27 +0300

    added command w to check the weater

--- src/utils/bin/weather.ts
+++ src/utils/bin/weather.ts
@@ -11,3 +11,15 @@ export const weather = async (args: string[]): Promise<string> => {
 
   return weather;
 };
+
+export const w = async (args: string[]): Promise<string> => {
+  const city = args.join('+');
+
+  if (!city) {
+    return 'Usage: weather [city]. Example: weather casablanca';
+  }
+
+  const weather = await getWeather(city);
+
+  return weather;
+};
